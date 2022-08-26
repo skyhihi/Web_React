0662d4b3a4d6e771c0c474aee3ced3875ed11c35
skyhihi <fahja246@gmail.com>
2022-08-26 11:47:48 +0700

    add func-component.js

---
+++ src/App.js
@@ -0,0 +1,15 @@
+//import logo from './logo.svg';
+import './App.css';
+import {Header,Content,Footer} from "./func-component"
+function App() {
+  
+  return (
+    <>
+      <Header/>,
+      <Content/>,
+      <Footer/>
+    </>
+  );
+}
+
+export default App;
