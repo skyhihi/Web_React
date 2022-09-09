db50797797fc1ba2d4c9eb0d0aaaae2a5b722fda
skyhihi <fahja246@gmail.com>
2022-09-09 12:48:12 +0700

    add Refs and State

--- src/App.js
+++ src/App.js
@@ -3,11 +3,12 @@ import "./App.css";
 //import { Header, Content, Footer } from "./func-component";
 //import MsgBox from "./class-props";
 //import Banner from "./banner";
-
-import {EventData2} from "./event-data"
+//import {EventData2} from "./event-data"
+//import RefsArray from "./refs-array";
+import MessageBox from "./state-func";
 
 function App() {
-  return <EventData2 />;
+  return <MessageBox />;
 }
 
 export default App;
